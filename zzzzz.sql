35fa62d20951b1643bc066326a4bae086740348a
YuukiNakai <55988240+n-ynkhh@users.noreply.github.com>
2024-02-08 13:39:49 +0900

    Update zzzzz.sql

--- zzzzz.sql
+++ zzzzz.sql
@@ -1,67 +1,64 @@
-WITH date_range AS (
+WITH recursive date_series AS (
   SELECT
-    MIN(DATE_TRUNC('MONTH', creation_date)) AS start_date,
-    MAX(DATE_TRUNC('MONTH', creation_date)) AS end_date
+    MIN(DATE_TRUNC('MONTH', creation_date)) AS check_date
   FROM
     task_table
-),
-monthly_dates AS (
+  UNION ALL
   SELECT
-    DATEADD('MONTH', SEQ4(), start_date) AS check_date
+    DATEADD('MONTH', 1, check_date)
   FROM
-    date_range,
-    TABLE(GENERATOR(ROWCOUNT => 1 + DATEDIFF('MONTH', start_date, end_date))) v
+    date_series
+  WHERE
+    check_date < CURRENT_DATE()
 )
 SELECT
   check_date AS month,
-  (SELECT COUNT(*) FROM task_table WHERE (completion_date IS NULL OR completion_date >= check_date)) AS incomplete_tasks_count
+  (SELECT COUNT(*) FROM task_table WHERE completion_date IS NULL OR completion_date > check_date) AS incomplete_tasks_count
 FROM
-  monthly_dates
+  date_series
 ORDER BY
   month;
 
 
-WITH date_range AS (
+WITH recursive date_series AS (
   SELECT
-    MIN(DATE_TRUNC('WEEK', creation_date)) AS start_date,
-    MAX(DATE_TRUNC('WEEK', creation_date)) AS end_date
+    MIN(DATE_TRUNC('WEEK', creation_date)) AS check_date
   FROM
     task_table
-),
-weekly_dates AS (
+  UNION ALL
   SELECT
-    DATEADD('WEEK', SEQ4(), start_date) AS check_date
+    DATEADD('WEEK', 1, check_date)
   FROM
-    date_range,
-    TABLE(GENERATOR(ROWCOUNT => 1 + DATEDIFF('WEEK', start_date, end_date))) v
+    date_series
+  WHERE
+    check_date < CURRENT_DATE()
 )
 SELECT
   check_date AS week,
-  (SELECT COUNT(*) FROM task_table WHERE (completion_date IS NULL OR completion_date >= check_date)) AS incomplete_tasks_count
+  (SELECT COUNT(*) FROM task_table WHERE completion_date IS NULL OR completion_date > check_date) AS incomplete_tasks_count
 FROM
-  weekly_dates
+  date_series
 ORDER BY
   week;
 
-
-WITH date_range AS (
+WITH recursive date_series AS (
   SELECT
-    MIN(creation_date) AS start_date,
-    MAX(creation_date) AS end_date
+    MIN(creation_date)::DATE AS check_date
   FROM
     task_table
-),
-daily_dates AS (
+  UNION ALL
   SELECT
-    DATEADD('DAY', SEQ4(), start_date) AS check_date
+    check_date + INTERVAL '1 day'
   FROM
-    date_range,
-    TABLE(GENERATOR(ROWCOUNT => 1 + DATEDIFF('DAY', start_date, end_date))) v
+    date_series
+  WHERE
+    check_date < CURRENT_DATE()
 )
 SELECT
   check_date AS day,
-  (SELECT COUNT(*) FROM task_table WHERE (completion_date IS NULL OR completion_date >= check_date)) AS incomplete_tasks_count
+  (SELECT COUNT(*) FROM task_table WHERE completion_date IS NULL OR completion_date > check_date) AS incomplete_tasks_count
 FROM
-  daily_dates
+  date_series
 ORDER BY
   day;
+
