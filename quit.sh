12108e4e7ff6636c1361a50c16db8fae304e7038
YuukiNakai <55988240+n-ynkhh@users.noreply.github.com>
2024-02-13 11:14:28 +0900

    Update quit.sh

--- quit.sh
+++ quit.sh
@@ -2,3 +2,10 @@ START_DATE=$(date -u -d "$YEAR_MONTH-01" '+%Y-%m-%dT%H:%M:%SZ')
 
 # 指定された年月の最終日の23:59を日本時間で計算し、UTCに変換
 END_DATE=$(date -u -d "$YEAR_MONTH-01 +1 month -1 day 9 hours" '+%Y-%m-%dT%H:%M:%SZ')
+
+
+# JSTで指定された年月の初日の0時をUTCに変換（9時間引く）
+START_DATE=$(date -u -d "$YEAR_MONTH-01 00:00:00 JST -9 hours" '+%Y-%m-%dT%H:%M:%SZ')
+
+# JSTで指定された年月の最終日の23時59分をUTCに変換（9時間引く）
+END_DATE=$(date -u -d "$YEAR_MONTH-01 00:00:00 JST +1 month -1 second -9 hours" '+%Y-%m-%dT%H:%M:%SZ')
